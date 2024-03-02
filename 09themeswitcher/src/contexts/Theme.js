bedfd02c59f0ec34b82c14e6604dc6be2b942a2d
[samiran1605] <[samthemaximum@gmail.com]>
2024-03-02 12:10:44 +0530

    formating fixed

--- 09themeswitcher/src/contexts/Theme.js
+++ 09themeswitcher/src/contexts/Theme.js
@@ -4,11 +4,11 @@ export const ThemeContext = createContext({
     themeMode: "light",
     darkTheme: ()=>{},
     lightTheme:()=>{}
-})
+});
 
 
-export const ThemeProvider = ThemeContext.Provider
+export const ThemeProvider = ThemeContext.Provider;
 
 export default function useTheme(){
     return useContext(ThemeContext)
-}
+};
