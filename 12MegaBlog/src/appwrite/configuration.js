5ee91dd633bfc59381235f95a32f841637c71b47
[samiran1605] <[samthemaximum@gmail.com]>
2024-03-06 09:54:08 +0530

    auth + redux added

---
+++ 12MegaBlog/src/appwrite/configuration.js
@@ -0,0 +1,126 @@
+import config from "../config/config";
+import { Client, Account, ID, Databases, Storage, Query } from "appwrite";
+
+export class Service {
+    client = new Client();
+    databases;
+    bucket;
+
+    constructor() {
+        this.client
+            .setEndpoint(config.appwriteUrl)
+            .setProject(config.appwriteProjectId);
+        this.databases = new Databases(this.client);
+        this.bucket = new Storage(this.client);
+    }
+
+    async createPost(slug, { title, content, featuredImage, status, userId }) {
+        try {
+            return await this.databases.createDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId
+                }
+            )
+        } catch (error) {
+            console.log("appwrite:: Service :: createPost", error);
+        }
+    }
+
+    async updatePost({ title, slug, content, featuredImage, status }) {
+        try {
+            await this.databases.updateDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status
+                }
+            )
+            return true;
+        } catch (error) {
+            console.log("appwrite:: Service :: updatePost", error)
+            return false;
+        }
+    }
+
+    async deletePost(slug) {
+        try {
+            return await this.databases.deleteDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug)
+        } catch (error) {
+            console.log("appwrite:: service: deletePost", error)
+        }
+    }
+
+    async getPost(slug) {
+        try {
+            return await this.databases.getDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug
+            )
+        } catch (error) {
+            console.log("appwrite:: service: getPost", error)
+            return false
+        }
+    }
+
+    async getPosts(queries = [Query.equal("status", "active")]) {
+        try {
+            return await this.databases.listDocuments(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                queries,
+            )
+        } catch (error) {
+            console.log("appwrite:: service: getPosts(all post)", error);
+            return false
+        }
+    }
+
+    // File upload services
+    async uploadFile(file) {
+        try {
+            return await this.bucket.createFile(
+                config.appwriteBucketId,
+                ID.unique(),
+                file,
+            )
+        } catch (error) {
+            console.log("appwrite:: service: uploadFile", error)
+            return false
+        }
+    }
+
+    async deleteFile(fileId) {
+        try {
+            await this.bucket.deleteFile(
+                config.appwriteBucketId,
+                fileId
+            )
+            return true
+        } catch (error) {
+            console.log("appwrite:: service: deleteFile", error)
+            return false
+        }
+    }
+
+    getFilePreview(fileId) {
+        return this.bucket.getFilePreview(
+            config.appwriteBucketId,
+            fileId
+        )
+    }
+}
